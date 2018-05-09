492b54bbfc762a375b7e44b200a2d37e87b55983
evglevin <levin.inf@gmail.com>
2018-05-10 01:11:57 +0300

    Person Detail Screen added
    Phone calls added. Supports numbers like:
    
    +1(222)333-44-55
    +42 555.123.4567
    +1-(800)-123-4567
    +7 555 1234567
    +7(926)1234567
    (926) 1234567
    +79261234567
    926 1234567
    9261234567
    1234567
    123-4567
    123-89-01
    495 1234567
    469 123 45 67
    8 (926) 1234567
    89261234567
    926.123.4567
    415-555-1234
    650-555-2345
    (416)555-3456
    202 555 4567
    4035555678
    1 416 555 9292

--- Faces/Swift/ViewControllers/Settings/People/PeopleTableViewController.swift
+++ Faces/Swift/ViewControllers/Settings/People/PeopleTableViewController.swift
@@ -93,17 +93,22 @@ class PeopleTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "personDetailSegue" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let destinationViewController = segue.destination as! PersonDetailTableViewController
+                destinationViewController.person = self.people[indexPath.row]
+            }
+        }
     }
-    */
-
 }
+
+
 
 extension PeopleTableViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
---
+++ Faces/Swift/ViewControllers/Settings/People/PersonDetailTableViewController.swift
@@ -0,0 +1,160 @@
+//
+//  PersonDetailTableViewController.swift
+//  Faces
+//
+//  Created by Евгений Левин on 10.05.2018.
+//  Copyright © 2018 levin inc. All rights reserved.
+//
+
+import UIKit
+
+class PersonDetailTableViewController: UITableViewController {
+    @IBOutlet weak var faceImageView: UIImageView!
+    @IBOutlet weak var phoneCell: UITableViewCell!
+    @IBOutlet weak var emailCell: UITableViewCell!
+    @IBOutlet weak var informationCell: UITableViewCell!
+    
+    var person: Person?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        self.navigationItem.title = person?.name
+        self.faceImageView.image = UIImage(contentsOfFile: documentsURL.appendingPathComponent(SettingsManager.getModelPath() + "/Avatars/" + (person?.photoTitle!)!).path)
+        faceImageView.layer.cornerRadius = 35
+        faceImageView.clipsToBounds = true
+        
+        self.phoneCell.textLabel?.text = person?.phone
+        self.emailCell.textLabel?.text = person?.email
+        self.informationCell.textLabel?.text = person?.information
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    
+        // Configure the cell...
+    
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 1:
+            showPhoneAlert()
+            tableView.deselectRow(at: indexPath, animated: true)
+        default:
+            tableView.deselectRow(at: indexPath, animated: false)
+        }
+        
+    }
+    
+    func showPhoneAlert() {
+        let ac = UIAlertController(title: nil, message: "Select an action", preferredStyle: .actionSheet)
+        print(person?.phone ?? "")
+        let call = UIAlertAction(title: "Call: \(person?.phone ?? "")", style: .default, handler: { _ in
+            self.person?.phone?.makeAColl()
+        })
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        ac.addAction(cancel)
+        ac.addAction(call)
+        present(ac, animated: true, completion: nil)
+    }
+
+}
+
+extension String {
+    
+    enum RegularExpressions: String {
+        case phone = "^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$"
+    }
+    
+    func isValid(regex: RegularExpressions) -> Bool {
+        return isValid(regex: regex.rawValue)
+    }
+    
+    func isValid(regex: String) -> Bool {
+        let matches = range(of: regex, options: .regularExpression)
+        return matches != nil
+    }
+    
+    func onlyDigits() -> String {
+        let filtredUnicodeScalars = unicodeScalars.filter{CharacterSet.decimalDigits.contains($0)}
+        return String(String.UnicodeScalarView(filtredUnicodeScalars))
+    }
+    
+    func makeAColl() {
+        if isValid(regex: .phone) {
+            if let url = URL(string: "tel://\(self.onlyDigits())"), UIApplication.shared.canOpenURL(url) {
+                if #available(iOS 10, *) {
+                    UIApplication.shared.open(url)
+                } else {
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        }
+    }
+}
